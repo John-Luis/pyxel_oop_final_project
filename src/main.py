c599d2c03ab936b01450472c743c2c3bbc5b89b4
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-06-20 21:08:09 +0800

    Final submission: Vortexion retro game engine structure

---
+++ src/main.py
@@ -0,0 +1,47 @@
+# title: Vortexion
+# author: Adam
+# desc: An MSX/SG-1000 inspired shoot-em-up
+# site: https://github.com/helpcomputer/vortexion
+# license: MIT
+# version: 1.0
+
+import pyxel as px
+from game import Game
+
+from const import APP_WIDTH, APP_HEIGHT, APP_NAME, APP_DISPLAY_SCALE, \
+    APP_CAPTURE_SCALE, APP_FPS, APP_GFX_FILE, PALETTE, SOUNDS_RES_FILE
+from monospace_bitmap_font import MonospaceBitmapFont
+from input import Input
+
+class App:
+    def __init__(self) -> None:
+        px.init(
+            APP_WIDTH, APP_HEIGHT,
+            title=APP_NAME,
+            fps=APP_FPS,
+            display_scale=APP_DISPLAY_SCALE,
+            capture_scale=APP_CAPTURE_SCALE
+        )
+
+        px.colors[:] = PALETTE
+        px.images[0].load(0, 0, "assets/" + APP_GFX_FILE)
+        px.load("assets/" + SOUNDS_RES_FILE, exclude_images=True,
+                exclude_tilemaps=True, exclude_musics=True)
+
+        self.main_font = MonospaceBitmapFont()
+        self.input = Input()
+
+        self.game = Game(self)
+
+        px.run(self.update, self.draw)
+
+    def update(self):
+        self.input.update()
+        self.game.update()
+
+    def draw(self):
+        px.cls(0)
+        self.game.draw()
+
+if __name__ == "__main__":
+    App()
